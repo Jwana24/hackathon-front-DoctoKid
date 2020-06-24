8a6c80976ba12442207d3c4c58c51e7910ef365c
Jwana24 <detrieux.btscom13@gmail.com>
2020-06-24 15:13:35 +0200

    first commit

---
+++ hackathon-front-doctokid/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Coucou : Johanna c'est la meilleure
+    </div>
+  );
+}
+
+export default App;
