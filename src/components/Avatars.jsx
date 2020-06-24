026fbf9f0f7cac66a576183a4ebe704430613684
Thomas <thomas.lachieze@gmail.com>
2020-06-24 19:30:08 +0200

    adding modals

--- src/components/Avatars.jsx
+++ src/components/Avatars.jsx
@@ -1,59 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import Dino1 from '../img/dino1.png';
 import Dino2 from '../img/dino2.png';
 import Dino3 from '../img/dino3.png';
 import Dino4 from '../img/dino4.png';
 import '../App.css';
+import { Button, Modal } from 'react-bootstrap';
 
 const Avatars = () => {
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
   return (
-    <section className="avatars-body container">
-      <h2 className="mb-4 text-center">Choisi ton Dino !</h2>
-      <div className="card-group">
-        <div className="card-body card1 col-md-3">
-          <div className="card-img">
-            <img src={Dino1} alt="Dino1"/>
+    <>
+      <Modal size="md" show={show} onHide={() => handleClose()} centered>
+        <Modal.Body>
+          Es-tu sûr de choisir ce Dino ?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="outline-secondary" onClick={() => handleClose()}>
+            Non
+          </Button>
+          <Button variant="outline-danger">
+            Oui !
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <section className="avatars-body container">
+        <h2 className="mb-4 text-center">Choisi ton Dino !</h2>
+        <div className="card-group">
+          <div className="card-body card1 col-md-3">
+            <div className="card-img">
+              <img src={Dino1} alt="Dino1"/>
+            </div>
+            <div className="text-center">
+            <button className="button-avatar" onClick={() => setShow(true)}>
+              A moi !
+            </button>
+            </div>
           </div>
-          <div className="text-center">
-          <button className="button-avatar">
-            A moi !
-          </button>
+          <div className="card-body card2 col-md-3">
+            <div className="card-img">
+              <img src={Dino2} alt="Dino2"/>
+            </div>
+            <div className="text-center">
+            <button className="button-avatar" onClick={() => setShow(true)}>
+              A moi !
+            </button>
+            </div>
+          </div>
+          <div className="card-body card3 col-md-3">
+            <div className="card-img">
+              <img src={Dino3} alt="Dino3"/>
+            </div>
+            <div className="text-center">
+            <button className="button-avatar" onClick={() => setShow(true)}>
+              A moi !
+            </button>
+            </div>
+          </div>
+          <div className="card-body card4 col-md-3">
+            <div className="card-img">
+              <img src={Dino4} alt="Dino4"/>
+            </div>
+            <div className="text-center">
+            <button className="button-avatar" onClick={() => setShow(true)}>
+              A moi !
+            </button>
+            </div>
           </div>
         </div>
-        <div className="card-body card2 col-md-3">
-          <div className="card-img">
-            <img src={Dino2} alt="Dino2"/>
-          </div>
-          <div className="text-center">
-          <button className="button-avatar">
-            A moi !
-          </button>
-          </div>
-        </div>
-        <div className="card-body card3 col-md-3">
-          <div className="card-img">
-            <img src={Dino3} alt="Dino3"/>
-          </div>
-          <div className="text-center">
-          <button className="button-avatar">
-            A moi !
-          </button>
-          </div>
-        </div>
-        <div className="card-body card4 col-md-3">
-          <div className="card-img">
-            <img src={Dino4} alt="Dino4"/>
-          </div>
-          <div className="text-center">
-          <button className="button-avatar">
-            A moi !
-          </button>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
