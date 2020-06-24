6ca46c401d23b5b8349eeb6a75b29a826c07ebd6
Jwana24 <detrieux.btscom13@gmail.com>
2020-06-24 22:14:02 +0200

    add deseases informations page

--- src/components/Informations.jsx
+++ src/components/Informations.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
+
+import './Informations';
 
 const Informations = () => {
+  const [ deseases, setDeseases ] = useState([]);
+
+  useEffect(() => {
+    Axios({
+      method: 'GET',
+      url: 'http://localhost:3000/api/deseases', // `http://localhost:3000/api/deseases?name=${name}`
+      data: deseases
+    })
+    .then(res => res.data)
+    .then(data => setDeseases(data))
+  }, []);
+
   return(
-    <div></div>
-  )
-}
+    <div className="Info">
+      <h1>Deseases informations</h1>
+      <ul>
+          {deseases.map((desease, id) => (
+            <div key={id}>
+              <li><h2>{desease.name}</h2></li>
+              <iframe type="text/html" width="800px" height="500px" src={desease.video} />
+            </div>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Informations;
