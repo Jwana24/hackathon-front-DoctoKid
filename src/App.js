1d5f41378623db61c861f7d5c575ce9556ce8b17
PierreVelluet <velluetp@gmail.com>
2020-06-24 20:41:32 +0200

    job done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Signin from './components/Signin';
 import Avatars from './components/Avatars';
-<<<<<<< HEAD
-import quizz from './components/Quiz'
-=======
-import Quiz from './components/Quiz';
+import Quizz from './components/Quizz/Quizz';
 import Informations from './components/Informations';
->>>>>>> a19d4016e13f52abbf4f4ecca99562372b90bb4b
 
 function App() {
   return (
@@ -17,7 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Signin} />
         <Route path="/avatars" component={Avatars} />
-        <Route path="/quiz" component={Quiz} />
+        <Route path="/quizz" component={Quizz} />
         <Route path="/informations" component={Informations} />
       </Switch>
     </>
