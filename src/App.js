184a483a4f01f6c65a4e5c756513c71610b8d3c1
Jwana24 <detrieux.btscom13@gmail.com>
2020-06-24 17:16:49 +0200

    fix conflicts import Quiz

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Signin from './components/Signin';
 import Avatars from './components/Avatars';
-<<<<<<< HEAD
-import quizz from './components/Quiz'
-=======
 import Quiz from './components/Quiz';
 import Informations from './components/Informations';
->>>>>>> a19d4016e13f52abbf4f4ecca99562372b90bb4b
 
 function App() {
   return (
