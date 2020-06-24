31934d0f363a8f8b4b344b4de45adb87bb6b3bcd
Thomas <thomas.lachieze@gmail.com>
2020-06-24 23:47:35 +0200

    Adding display of puzzle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,21 +6,55 @@ import Signup from './components/Signup';
 import Signin from './components/Signin';
 import Avatars from './components/Avatars';
 import Quizz from './components/Quizz/Quizz';
-
 import Informations from './components/Informations';
+import { ImagesProvider } from "./components/contexts/ImageContext";
+import { useTransition, animated } from "react-spring";
+import useRouter from "./hooks/useRouter";
 
-function App() {
-  return (
+const Puzzle = React.lazy(() => {
+  return import("./components/Puzzle/Puzzle");
+});
+
+const  App = props => {
+  const { location } = useRouter();
+  const transitions = useTransition(location, location => location.pathname, {
+    from: {
+      opacity: location.pathname !== "/" ? 0 : 1,
+      transform: "translateX(0%)"
+    },
+    enter: { opacity: 1, transform: "translateX(0%)" },
+    leave: { opacity: 0, transform: "translateX(-20%)", delay: 0 }
+  });
+  return transitions.map(({ item, props, key }) => (
     <>
-      <Switch>
-        <Route exact path="/" component={Signup} />
-        <Route exact path="/signin" component={Signin} />
-        <Route path="/avatars" component={Avatars} />
-        <Route path="/quizz" component={Quizz} />
-        <Route path="/informations" component={Informations} />
-      </Switch>
+      <animated.div key={key} style={props}>
+        <Suspense fallback={""}>
+          <Switch location={item}>
+            <Route exact path="/" component={Signup} />
+            <Route exact path="/signin" component={Signin} />
+            <Route path="/avatars" component={Avatars} />
+            <Route path="/quizz" component={Quizz} />
+            <Route
+              path="/puzzle"
+              exact
+              render={props => (
+                <ImagesProvider
+                  r={require.context(
+                    "./components/Puzzle/images",
+                    false,
+                    /\.(png|jpe?g|svg)$/
+                  )}
+                >
+                  <Puzzle {...props} />
+                </ImagesProvider>
+              )}
+            />
+            <Route path="/informations" component={Informations} />
+          </Switch>
+        </Suspense>
+      </animated.div>
     </>
-  );
+  ));
 }
 
 export default App;
